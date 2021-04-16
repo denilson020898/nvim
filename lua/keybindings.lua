ba8680eba4443822cbc539edb750edde62b941de
Denilson <denilson@mplus.software>
2021-04-16 17:38:50 +0700

    better neovide

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -45,12 +45,12 @@ vim.api.nvim_set_keymap("n", "", ':call NERDComment(0, "toggle")<CR>', {noremap
 vim.api.nvim_set_keymap("v", "", ':call NERDComment(0, "toggle")<CR>gv', {noremap = true, silent = true})
 
 vim.api.nvim_set_keymap("n", "<Leader>t", ":Telescope<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<Leader>tp", "<cmd>lua require('nvim-plugins.telescope').search_dotfiles()<CR>", {noremap = true, silent = true})
 vim.api.nvim_set_keymap("n", "<Leader>f", "<cmd>lua require('telescope.builtin').find_files()<CR>", {noremap = true, silent = true})
 vim.api.nvim_set_keymap("n", "<Leader>g", "<cmd>lua require('telescope.builtin').live_grep()<CR>", {noremap = true, silent = true})
 vim.api.nvim_set_keymap("n", "<Leader>b", "<cmd>lua require('telescope.builtin').buffers()<CR>", {noremap = true, silent = true})
 vim.api.nvim_set_keymap("n", "<Leader>h", "<cmd>lua require('telescope.builtin').help_tags()<CR>", {noremap = true, silent = true})
 
-vim.api.nvim_set_keymap("n", "<Leader>p", "<cmd>lua require('nvim-plugins.telescope').search_dotfiles()<CR>", {noremap = true, silent = true})
 vim.api.nvim_set_keymap("n", "<Leader>=", ":Neoformat<CR>", {noremap = true, silent = true})
 
 vim.api.nvim_set_keymap("n", "<Leader>qq", ":copen<CR>", {noremap = true, silent = true})
