327d4e92ee93c3befb71c43969b6c10360112cc4
Denilson <denilson020898@gmail.com>
2021-04-23 23:02:38 +0700

    some clean up before set up lsp

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,3 +1,2 @@
 -- require('lsp.global')
 require('lsp.rust_analyzer')
-require('lsp.saga')
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -10,41 +10,53 @@ end
 
 return require("packer").startup(function()
     -- Packer can manage itself
-    use "TimUntersberger/neogit"
-    use "glepnir/lspsaga.nvim"
-    use "hrsh7th/nvim-compe"
-    use "hrsh7th/vim-vsnip"
-    use "jnurmine/Zenburn"
-    use "kyazdani42/nvim-tree.lua"
-    use "nvim-lua/popup.nvim"
-    use "nvim-telescope/telescope-media-files.nvim"
-    use "nvim-telescope/telescope.nvim"
-    use "nvim-treesitter/playground"
-    use "p00f/nvim-ts-rainbow"
     use "wbthomason/packer.nvim"
+
+    -- ui
+    -- use 'kosayoda/nvim-lightbulb'
+    use 'hoob3rt/lualine.nvim'
+    use "kyazdani42/nvim-tree.lua"
+    use 'kyazdani42/nvim-web-devicons'
+    use "nvim-lua/popup.nvim"
+    use 'nvim-lua/plenary.nvim'
+    -- use {
+    --     'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' },
+    --     config = function() require('gitsigns').setup() end
+    -- }
+    use "kevinhwang91/nvim-bqf"
+    use 'norcalli/nvim-colorizer.lua'
+    use "p00f/nvim-ts-rainbow"
+    use 'akinsho/nvim-bufferline.lua'
+
+    -- color
+    use {"npxbr/gruvbox.nvim", requires = {"rktjmp/lush.nvim"}}
+    use "jnurmine/Zenburn"
+    use "nanotech/jellybeans.vim"
+
+    -- helpers
+    use 'nanotee/nvim-lua-guide'
+    use "hrsh7th/nvim-compe"
+    use 'sbdchd/neoformat'
+    use 'b3nj5m1n/kommentary'
+    use "hrsh7th/vim-vsnip"
+    use "TimUntersberger/neogit"
+    use "nvim-telescope/telescope.nvim"
+    use "nvim-telescope/telescope-media-files.nvim"
+    use { "nvim-treesitter/nvim-treesitter", run=":TSUpdate" }
+    use "nvim-treesitter/playground"
     use "windwp/nvim-autopairs"
     use 'airblade/vim-rooter'
-    use 'akinsho/nvim-bufferline.lua'
-    use 'b3nj5m1n/kommentary'
-    use 'kabouzeid/nvim-lspinstall'
-    use 'kosayoda/nvim-lightbulb'
-    use 'kyazdani42/nvim-web-devicons'
-    use 'mfussenegger/nvim-dap'
-    use 'mfussenegger/nvim-jdtls'
-    use 'nanotee/nvim-lua-guide'
-    use 'norcalli/nvim-colorizer.lua'
-    use 'onsails/lspkind-nvim'
-    use 'sbdchd/neoformat'
-    use { "nvim-treesitter/nvim-treesitter", run=":TSUpdate" }
-    use 'hoob3rt/lualine.nvim'
-    use {
-        'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' },
-        config = function() require('gitsigns').setup() end
-    }
-    use "kevinhwang91/nvim-bqf"
-    use "nanotech/jellybeans.vim"
+
+    -- lsp
     use "neovim/nvim-lspconfig"
+    use 'kabouzeid/nvim-lspinstall'
     use "nvim-lua/lsp_extensions.nvim"
-    use {"npxbr/gruvbox.nvim", requires = {"rktjmp/lush.nvim"}}
+    -- use "glepnir/lspsaga.nvim"
+    -- use 'onsails/lspkind-nvim'
+
+    -- debug
+    -- use 'mfussenegger/nvim-dap'
+    -- use 'mfussenegger/nvim-jdtls'
+
 end
 )
