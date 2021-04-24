0a62db92a967f9ca97265dfad340dcec0fbbe829
Denilson <denilson020898@gmail.com>
2021-04-24 13:31:08 +0700

    make terminal a window instead of float window

--- lua/nvim-plugins/toggleterm.lua
+++ lua/nvim-plugins/toggleterm.lua
@@ -1,13 +1,14 @@
 require"toggleterm".setup{
   -- size = 50,
-  open_mapping = [[<c-\>]],
+  open_mapping = [[<Leader>\]],
   hide_numbers = true, -- hide the number column in toggleterm buffers
   shade_filetypes = {},
   shade_terminals = true,
   shading_factor = 2, -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
   start_in_insert = true,
   persist_size = true,
-  direction = 'float',
+  -- direction = 'float', to make it a float window, probabro not worth it
+  direction = 'window',
   shell = vim.o.shell, -- change the default shell 
   -- This field is only relevant if direction is set to 'float'
   float_opts = {
