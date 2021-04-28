2ab5686481c27938a4d574cbd00e7bdefc971deb
Denilson <denilson@mplus.software>
2021-04-28 23:50:22 +0700

    toggleterm

--- lua/nvim-plugins/toggleterm.lua
+++ lua/nvim-plugins/toggleterm.lua
@@ -1,15 +1,15 @@
 require"toggleterm".setup{
-  -- size = 20,
+  -- size = 1,
   open_mapping = [[<Leader>\]],
   hide_numbers = true, -- hide the number column in toggleterm buffers
   shade_filetypes = {},
   shade_terminals = true,
   shading_factor = 1,
   start_in_insert = true,
-  persist_size = true,
+  persist_size = false,
   -- direction = 'vertical' | 'horizontal' | 'window' | 'float',
   direction = 'float',
-  shell = vim.o.shell, -- change the default shell 
+  shell = vim.o.shell, -- change the default shell
   -- This field is only relevant if direction is set to 'float'
   float_opts = {
     -- The border key is *almost* the same as 'nvim_win_open'
@@ -18,8 +18,8 @@ require"toggleterm".setup{
     -- not natively supported but implemented in this plugin.
     -- border = 'single' | 'double' | 'shadow' | 'curved' | ... other options supported by win open
     border = 'curved',
-    width = math.floor(vim.o.columns * 0.925),
-    height = math.floor(vim.o.lines * 0.9),
+    -- width = math.floor(vim.o.columns * 0.925),
+    -- height = math.floor(vim.o.lines * 0.9),
     winblend = 3,
     highlights = {
       border = "Normal",
