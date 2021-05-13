895b23412ea688de7174ecee1360209b61f2833f
Denilson <denilson020898@gmail.com>
2021-05-13 20:20:57 +0700

    nvim compe and vimsnip

--- lua/nvim-plugins/compe.lua
+++ lua/nvim-plugins/compe.lua
@@ -69,3 +69,23 @@ vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
 -- inoremap <silent><expr> <C-e>     compe#close("<C-e>")
 -- inoremap <silent><expr> <C-f>     compe#scroll({ "delta": +4 })
 -- inoremap <silent><expr> <C-d>     compe#scroll({ "delta": -4 })
+
+-- " Expand
+vim.cmd("imap <expr> <C-j>   vsnip#expandable()  ? '<Plug>(vsnip-expand)'         : '<C-j>'")
+vim.cmd("smap <expr> <C-j>   vsnip#expandable()  ? '<Plug>(vsnip-expand)'         : '<C-j>'")
+-- " Expand or jump
+vim.cmd("imap <expr> <C-l>   vsnip#available(1)  ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>'")
+vim.cmd("smap <expr> <C-l>   vsnip#available(1)  ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>'")
+
+-- " Jump forward or backward
+-- imap <expr> <Tab>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>'
+-- smap <expr> <Tab>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>'
+-- imap <expr> <S-Tab> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'
+-- smap <expr> <S-Tab> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)'      : '<S-Tab>'
+
+-- " Select or cut text to use as $TM_SELECTED_TEXT in the next snippet.
+-- " See https://github.com/hrsh7th/vim-vsnip/pull/50
+-- nmap        s   <Plug>(vsnip-select-text)
+-- xmap        s   <Plug>(vsnip-select-text)
+-- nmap        S   <Plug>(vsnip-cut-text)
+-- xmap        S   <Plug>(vsnip-cut-text)
