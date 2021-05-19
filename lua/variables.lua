675d74f778f57f48bdfecf0596150b85fc7aefe0
Denilson <denilson@mplus.software>
2021-05-19 09:59:38 +0700

    nvim tree increase width

--- lua/variables.lua
+++ lua/variables.lua
@@ -1,4 +1,7 @@
 vim.g.NERDSpaceDelims = 1
 vim.g.nvim_tree_ignore = { '.git', 'node_modules', '.cache' }
+vim.g.nvim_tree_git_hl = 1
+vim.g.nvim_tree_width = 69
+vim.g.nvim_tree_width_allow_resize = 1
 -- vim.g.neovide_transparency=1.0
 -- vim.g.neovide_fullscreen = true
