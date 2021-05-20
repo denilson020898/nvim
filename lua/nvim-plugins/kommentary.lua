7b7cd28e0418b32bfec65318c07b979ffbabecd9
Denilson <denilson020898@gmail.com>
2021-05-20 14:51:41 +0700

    kommentary prefer single line, support for wast

--- lua/nvim-plugins/kommentary.lua
+++ lua/nvim-plugins/kommentary.lua
@@ -1,10 +1,10 @@
 require('kommentary.config').use_extended_mappings()
 
-local languages = {"rust", "javascript", "lua", "python3", "html"}
+require('kommentary.config').configure_language("wast", {
+    single_line_comment_string = ";;",
+    multi_line_comment_strings = {"(;", ";)"},
+})
 
-for i, lang in ipairs(languages) do
-    require('kommentary.config').configure_language(lang, {
-        prefer_single_line_comments = true,
-    })
-end
-
+require('kommentary.config').configure_language("default", {
+    prefer_single_line_comments = true,
+})
