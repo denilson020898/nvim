0457fdc25d8ea69bab60228c22579160b71ec87d
Denilson <denilson020898@gmail.com>
2021-05-26 17:05:46 +0700

    initiate rust_tools first, then override it with our rust_analyzer settings

--- lua/lsp/rust_analyzer.lua
+++ lua/lsp/rust_analyzer.lua
@@ -1,39 +1,3 @@
-local nvim_lsp = require('lspconfig')
-local on_attach = require("lsp.global").on_attach
-local capabilities = require("lsp.global").capabilities
-
-nvim_lsp.rust_analyzer.setup({ 
-    -- not ready yet in windows for using automatic lspinstall
-    -- cmd = { vim.fn.stdpath("data").."/lspinstall/rust/rust-analyzer" },
-    on_attach=on_attach,
-    capabilities = capabilities
-})
-
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-    vim.lsp.diagnostic.on_publish_diagnostics, {
-        virtual_text = true,
-        signs = true,
-        update_in_insert = true,
-    }
-)
-
-vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
-    vim.lsp.handlers.signature_help, {
-        border = "single"
-    }
-)
-
-vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
-    vim.lsp.handlers.hover, {
-        border = "single"
-    }
-)
-
--- vim.api.nvim_exec([[
--- autocmd CursorMoved,InsertLeave,BufEnter,BufWinEnter,TabEnter,BufWritePost * lua require'lsp_extensions'.inlay_hints{aligned = false, prefix = ' -- ', highlight = "Comment", enabled = {"TypeHint", "ChainingHint", "ParameterHint"} }
--- autocmd CursorHold * lua vim.lsp.diagnostic.show_line_diagnostics()
--- ]], false)
-
 local opts = {
     tools = { -- rust-tools options
         -- automatically set inlay hints (type hints)
@@ -105,3 +69,41 @@ local opts = {
 }
 
 require('rust-tools').setup(opts)
+
+
+local nvim_lsp = require('lspconfig')
+local on_attach = require("lsp.global").on_attach
+local capabilities = require("lsp.global").capabilities
+
+nvim_lsp.rust_analyzer.setup({ 
+    -- not ready yet in windows for using automatic lspinstall
+    -- cmd = { vim.fn.stdpath("data").."/lspinstall/rust/rust-analyzer" },
+    on_attach=on_attach,
+    capabilities = capabilities
+})
+
+-- vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+--     vim.lsp.diagnostic.on_publish_diagnostics, {
+--         virtual_text = true,
+--         signs = true,
+--         update_in_insert = true,
+--     }
+-- )
+
+-- vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+--     vim.lsp.handlers.signature_help, {
+--         border = "single"
+--     }
+-- )
+
+-- vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+--     vim.lsp.handlers.hover, {
+--         border = "single"
+--     }
+-- )
+
+-- vim.api.nvim_exec([[
+-- autocmd CursorMoved,InsertLeave,BufEnter,BufWinEnter,TabEnter,BufWritePost * lua require'lsp_extensions'.inlay_hints{aligned = false, prefix = ' -- ', highlight = "Comment", enabled = {"TypeHint", "ChainingHint", "ParameterHint"} }
+-- autocmd CursorHold * lua vim.lsp.diagnostic.show_line_diagnostics()
+-- ]], false)
+
