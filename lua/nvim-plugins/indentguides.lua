133fc2c3373e9470c9402474ed821aca7a7cee1e
Denilson <denilson020898@gmail.com>
2021-05-29 10:25:01 +0700

    change indent guide color

--- lua/nvim-plugins/indentguides.lua
+++ lua/nvim-plugins/indentguides.lua
@@ -9,6 +9,6 @@ require('indent_guides').setup({
     indent_tab_guides = false;
     indent_soft_pattern = '\\s';
     exclude_filetypes = {'help','dashboard','dashpreview','NvimTree','vista','sagahover'};
-    even_colors = { fg ='#2a3834',bg='#332b36' };
-    odd_colors = {fg='#332b36',bg='#2a3834'};
+    even_colors = { fg ='#2b2b2b',bg='#2b2b2b' };
+    odd_colors = { fg ='#2b2b2b',bg='#2b2b2b' };
 })
