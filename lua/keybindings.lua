a1b22877131719018fe595fd14e3f9a8c6a54e23
Denilson <denilson020898@gmail.com>
2021-06-23 10:19:05 +0700

    change telescope buffer keybinding

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -53,7 +53,7 @@ vim.api.nvim_set_keymap("n", "<Leader>tp", "<cmd>lua require('nvim-plugins.teles
 vim.api.nvim_set_keymap("n", "<Leader>tx", "<cmd>lua require('nvim-plugins.telescope').search_with_extension()<CR>", {noremap = true, silent = true})
 vim.api.nvim_set_keymap("n", "<Leader>tf", "<cmd>lua require('telescope.builtin').find_files()<CR>", {noremap = true, silent = true})
 vim.api.nvim_set_keymap("n", "<Leader>tg", "<cmd>lua require('telescope.builtin').live_grep()<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<Leader>tb", "<cmd>lua require('telescope.builtin').buffers()<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<Leader>b", "<cmd>lua require('telescope.builtin').buffers()<CR>", {noremap = true, silent = true})
 vim.api.nvim_set_keymap("n", "<Leader>th", "<cmd>lua require('telescope.builtin').help_tags()<CR>", {noremap = true, silent = true})
 vim.api.nvim_set_keymap("n", "<Leader>ts", ":SearchSession<CR>", {noremap = true, silent = true})
 
