ef163641127e2604b637e223d63f60b689431355
Denilson <denilson020898@gmail.com>
2021-07-12 15:12:32 +0700

    telescope display path

--- lua/nvim-plugins/sessionlense.lua
+++ lua/nvim-plugins/sessionlense.lua
@@ -1,6 +1,6 @@
 require('session-lens').setup {
     path_display = {'absolute'},
-    shorten_path = false,
+    -- shorten_path = false,
     -- theme_conf = { border = true },
     -- previewer = false
 }
