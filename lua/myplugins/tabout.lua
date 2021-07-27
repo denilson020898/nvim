9fc0815efdd3b05d2c9218b1f2998a2aec0a30c2
Denilson <denilson020898@gmail.com>
2021-07-27 20:46:35 +0700

    double tabout key

--- lua/myplugins/tabout.lua
+++ lua/myplugins/tabout.lua
@@ -1,6 +1,6 @@
 require('tabout').setup({
     -- tabkey = "<Tab>", -- key to trigger tabout
-    tabkey = "<C-e>", -- key to trigger tabout
+    tabkey = "<C-u>", -- key to trigger tabout
     act_as_tab = true, -- shift content if tab out is not possible
     completion = true, -- if the tabkey is used in a completion pum
     tabouts = {
@@ -15,4 +15,6 @@ require('tabout').setup({
     ignore_beginning = true, --[[ if the cursor is at the beginning of a filled element it will rather tab out than shift the content ]]
     exclude = {} -- tabout will ignore these filetypes
 })
+
+vim.api.nvim_set_keymap("i", "<C-r>", "<cmd>Tabout<cr>", {})
 
