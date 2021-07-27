02ff1a693705997b89f3dc44d6193cf58e88659d
Denilson <denilson020898@gmail.com>
2021-07-27 20:39:20 +0700

    change tabout key

--- lua/myplugins/tabout.lua
+++ lua/myplugins/tabout.lua
@@ -1,5 +1,6 @@
 require('tabout').setup({
-    tabkey = "<Tab>", -- key to trigger tabout
+    -- tabkey = "<Tab>", -- key to trigger tabout
+    tabkey = "<C-e>", -- key to trigger tabout
     act_as_tab = true, -- shift content if tab out is not possible
     completion = true, -- if the tabkey is used in a completion pum
     tabouts = {
@@ -8,7 +9,8 @@ require('tabout').setup({
         {open = '`', close = '`'},
         {open = '(', close = ')'},
         {open = '[', close = ']'},
-        {open = '{', close = '}'}
+        {open = '{', close = '}'},
+        {open = '<', close = '>'}
     },
     ignore_beginning = true, --[[ if the cursor is at the beginning of a filled element it will rather tab out than shift the content ]]
     exclude = {} -- tabout will ignore these filetypes
