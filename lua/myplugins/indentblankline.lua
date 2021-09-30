34cc7da624baa376e202ac59dea0459d531be039
Denilson <denilson020898@gmail.com>
2021-09-30 13:56:50 +0700

    indent blank line setting

--- lua/myplugins/indentblankline.lua
+++ lua/myplugins/indentblankline.lua
@@ -1,16 +1,41 @@
-vim.g.indent_blankline_enabled = false
+-- vim.g.indent_blankline_enabled = false
 
-vim.opt.termguicolors = true
-vim.cmd [[highlight IndentBlanklineContextChar guifg=#990000 gui=nocombine]]
+-- vim.opt.termguicolors = true
+-- vim.cmd [[highlight IndentBlanklineContextChar guifg=#990000 gui=nocombine]]
+
+-- vim.opt.list = true
+-- vim.opt.listchars = {
+--     -- space = "⋅",
+--     eol = "↴",
+-- }
+
+-- require("indent_blankline").setup {
+--     space_char_blankline = " ",
+--     show_current_context = true,
+-- }
+
+-- vim.opt.termguicolors = true
+-- vim.cmd [[highlight IndentBlanklineIndent1 guibg=#1f1f1f gui=nocombine]]
+-- vim.cmd [[highlight IndentBlanklineIndent2 guibg=#1a1a1a gui=nocombine]]
+
+-- require("indent_blankline").setup {
+--     char = " ",
+--     char_highlight_list = {
+--         "IndentBlanklineIndent1",
+--         "IndentBlanklineIndent2",
+--     },
+--     space_char_highlight_list = {
+--         "IndentBlanklineIndent1",
+--         "IndentBlanklineIndent2",
+--     },
+--     show_trailing_blankline_indent = false,
+-- }
 
 vim.opt.list = true
-vim.opt.listchars = {
-    -- space = "⋅",
-    eol = "↴",
-}
+vim.opt.listchars:append("space:⋅")
+vim.opt.listchars:append("eol:↴")
 
 require("indent_blankline").setup {
     space_char_blankline = " ",
     show_current_context = true,
 }
-
