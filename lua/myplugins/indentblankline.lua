57f6ac9d365cb2a3aabfbe88910ca60fbd222fca
Denilson <denilson020898@gmail.com>
2021-09-30 14:00:47 +0700

    change indent blankline context color

--- lua/myplugins/indentblankline.lua
+++ lua/myplugins/indentblankline.lua
@@ -31,6 +31,7 @@
 --     show_trailing_blankline_indent = false,
 -- }
 
+vim.cmd [[highlight IndentBlanklineContextChar guifg=#FF0000 gui=nocombine]]
 vim.opt.list = true
 vim.opt.listchars:append("space:⋅")
 vim.opt.listchars:append("eol:↴")
