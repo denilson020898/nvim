1d92fe839f83b67653b4b01d6747bd6a5ce22f34
Denilson <denilson020898@gmail.com>
2021-12-11 09:23:22 +0700

    add sort

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -150,5 +150,17 @@ return require("packer").startup(function()
         end
     }
 
+    use({
+      'sQVe/sort.nvim',
+
+      -- Optional setup for overriding defaults.
+      config = function()
+        require("sort").setup({
+          -- Input configuration here.
+          -- Refer to the configuration section below for options.
+        })
+      end
+    })
+
 end
 )
