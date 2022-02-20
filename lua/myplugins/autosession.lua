978f652d6453c535bce16dbb942d18bbfa9767f8
denilson <denilson020898@gmail.com>
2022-02-21 02:30:40 +0700

    fix autosession for keybinding

--- lua/myplugins/autosession.lua
+++ lua/myplugins/autosession.lua
@@ -1,5 +1,15 @@
-require('auto-session').setup {
-    auto_session_enable_last_session = false,
-    log_level = 'info',
+local opts = {
+  log_level = 'info',
+  auto_session_enable_last_session = false,
+  -- auto_session_root_dir = vim.fn.stdpath('data').."/sessions/",
+  auto_session_enabled = true,
+  auto_save_enabled = nil,
+  auto_restore_enabled = nil,
+  auto_session_suppress_dirs = nil,
+  -- the configs below are lua only
+  bypass_session_save_file_types = nil
 }
 
+require('auto-session').setup(opts)
+
+vim.o.sessionoptions="blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal"
--- lua/variables.lua
+++ lua/variables.lua
@@ -7,13 +7,13 @@ vim.g.nvim_tree_width_allow_resize = 1
 -- vim.g.neovide_fullscreen = true
 -- vim.g.neovide_transparency=1.0
 
-vim.g.auto_session_post_restore_cmds = { 
-    "bd spectre",
+vim.g.auto_session_post_restore_cmds = {
+    "bd *spectre*",
 }
-vim.g.auto_session_pre_save_cmds = { 
-    "NvimTreeClose", 
-    "cclose", 
-    "AerialClose", 
+vim.g.auto_session_pre_save_cmds = {
+    "NvimTreeClose",
+    "cclose",
+    "AerialClose",
 }
 
 vim.g.gitblame_enabled = 0
@@ -21,11 +21,3 @@ vim.g.gitblame_enabled = 0
 -- Available options: <author>, <committer>, <date>, <committer-date>, <summary>, <sha>
 vim.g.gitblame_message_template = '<summary> • <author> • <date> • <sha>'
 
-vim.cmd([[
-if exists('g:fvim_loaded')
-    "FVimCursorSmoothMove v:true
-    "FVimCursorSmoothBlink v:true
-    FVimCustomTitleBar v:true
-    nnoremap <A-CR> :FVimToggleFullScreen<CR>
-    endif
-    ]])
