138f21cabbad6c260579551a8bc069605923114b
denilson <denilson020898@gmail.com>
2022-03-05 00:32:49 +0700

    change keys

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -113,7 +113,7 @@ vim.api.nvim_set_keymap("n", "<Leader>te", ":Telescope file_browser<CR>", {norem
 vim.api.nvim_set_keymap("n", "<Leader>tf", "<cmd>lua require('telescope.builtin').find_files()<CR>", {noremap = true})
 vim.api.nvim_set_keymap("n", "<Leader>tg", "<cmd>lua require('telescope.builtin').live_grep()<CR>", {noremap = true})
 vim.api.nvim_set_keymap("n", "<Leader>b", "<cmd>lua require('telescope.builtin').buffers()<CR>", {noremap = true})
-vim.api.nvim_set_keymap("n", "<Leader>y", "<cmd>lua require('telescope.builtin').buffers()<CR>", {noremap = true})
+vim.api.nvim_set_keymap("n", "<Leader>i", "<cmd>lua require('telescope.builtin').buffers()<CR>", {noremap = true})
 vim.api.nvim_set_keymap("n", "<Leader>th", "<cmd>lua require('telescope.builtin').help_tags()<CR>", {noremap = true})
 vim.api.nvim_set_keymap("n", "<Leader>ts", ":SearchSession<CR>", {noremap = true})
 
